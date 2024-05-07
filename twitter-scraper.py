dd852db7f325c43b027b47d3790952521305e566
clib <vitor.carvalho.ufu@gmail.com>
2024-05-07 15:55:52 -0300

    Collecting Tweet text | Response Tweet text | Has photo | Has video

--- twitter-scraper.py
+++ twitter-scraper.py
@@ -61,54 +61,92 @@ def login_via_google(new_page_info, login_data):
         print(f"Exception: No Google reCAPTCHA Pop Up - {e}")
 
 def extract_tweets_to_df(soup):
-    # Fetching all tags containing post links
-    post_info_a_tags = [a_tag.parent for a_tag in soup.select("a > time")] # Fetching all <a> tags that have time as a child (they are the specific ones for the tweets in the page, this filters the responses and other non related tweets)
-    post_time_elements = soup.select("a > time")
-    # Fetches Replies | Reposts | Likes | Bookmarks | Views
-    post_views_elements = soup.select("div[role=\"group\"]")
-    post_text_elements = soup.select("div[data-testid=\"tweetText\"]")
-    # Fetching all links and infos
+    tweet_elemets = soup.select("div[data-testid=\"cellInnerDiv\"]")
     base_url = "https://twitter.com"
-    post_links_lst     = [base_url+a_tag["href"] for a_tag in post_info_a_tags if (not "analytics" in a_tag["href"]) and (a_tag["href"].count("/") == 3)] # Number 3 here, becauce we add +2 with base url, so not 5, in fact 3
-    post_username_lst  = [a_tag["href"][1:a_tag["href"].find("/", 1)] for a_tag in post_info_a_tags if (not "analytics" in a_tag["href"]) and (a_tag["href"].count("/") == 3)]
-    post_date_lst      = [time_tag["datetime"][:time_tag["datetime"].find("T")] for time_tag in post_time_elements]
-    post_time_lst      = [time_tag["datetime"][time_tag["datetime"].find("T")+1:time_tag["datetime"].find(".")] for time_tag in post_time_elements]
+    post_links_lst     = [] 
+    post_username_lst  = []
+    post_date_lst      = []
+    post_time_lst      = []
     post_replies_lst   = []
     post_reposts_lst   = []
     post_likes_lst     = []
     post_bookmarks_lst = []
     post_views_lst     = []
-    post_text = []
+    post_text_lst = []
+    post_response_text_lst = []
+    post_has_photo_lst = []
+    post_has_video_lst = []
+    for tweet in tweet_elemets:
+        # Fetching all tags containing post links
+        post_info_a_tags = [a_tag.parent for a_tag in tweet.select("a > time")] # Fetching all <a> tags that have time as a child (they are the specific ones for the tweets in the page, this filters the responses and other non related tweets)
+        post_time_elements = tweet.select("a > time")
+        # Fetches Replies | Reposts | Likes | Bookmarks | Views
+        post_views_elements = tweet.select("div[role=\"group\"]")
+        post_text_elements = tweet.select("div[data-testid=\"tweetText\"]")
+        post_photo_elements = tweet.select("div[data-testid=\"tweetPhoto\"]")
+        post_video_elements = tweet.select("div[data-testid=\"videoComponent\"]")
+        # Fetching all links and infos
+        post_link = [base_url+a_tag["href"] for a_tag in post_info_a_tags if (not "analytics" in a_tag["href"]) and (a_tag["href"].count("/") == 3)] # Number 3 here, becauce we add +2 with base url, so not 5, in fact 3
+        post_username = [a_tag["href"][1:a_tag["href"].find("/", 1)] for a_tag in post_info_a_tags if (not "analytics" in a_tag["href"]) and (a_tag["href"].count("/") == 3)]
+        post_date = [time_tag["datetime"][:time_tag["datetime"].find("T")] for time_tag in post_time_elements]
+        post_time = [time_tag["datetime"][time_tag["datetime"].find("T")+1:time_tag["datetime"].find(".")] for time_tag in post_time_elements]        
 
-    for a_tag in post_views_elements:
-        replies, reposts, likes, bookmarks, views = [0 for _ in range(5)]
-        # Checking if multiple elements
-        if ", " in a_tag["aria-label"]:
-            infos_lst = a_tag["aria-label"].split(", ")
-            for info in infos_lst:
-                if "replie" in info:
-                    replies = int(info.split(" ")[0])  
-                elif "repost" in info:
-                    reposts = int(info.split(" ")[0])
-                elif "like" in info:
-                    likes = int(info.split(" ")[0])
-                elif "bookmark" in info:
-                    bookmarks = int(info.split(" ")[0])
-                elif "view" in info:
-                    views = int(info.split(" ")[0])
+        # We do not consider an element that does not have a tweet link (it's not a tweet)
+        if not post_link:
+            continue
 
-        post_replies_lst.append(replies)
-        post_reposts_lst.append(reposts)
-        post_likes_lst.append(likes)
-        post_bookmarks_lst.append(bookmarks)
-        post_views_lst.append(views)
+        post_links_lst.append(post_link[0])
+        post_username_lst.append(post_username[0])
+        post_date_lst.append(post_date[0])
+        post_time_lst.append(post_time[0])
 
-    for div in post_text_elements:
-        tweet_text = ""
-        if div.has_attr("id"):
-            for _, child in enumerate(div.children):
+        for a_tag in post_views_elements:
+            replies, reposts, likes, bookmarks, views = [0 for _ in range(5)]
+            # Checking if multiple elements
+            if ", " in a_tag["aria-label"]:
+                infos_lst = a_tag["aria-label"].split(", ")
+                for info in infos_lst:
+                    if "replie" in info:
+                        replies = int(info.split(" ")[0])  
+                    elif "repost" in info:
+                        reposts = int(info.split(" ")[0])
+                    elif "like" in info:
+                        likes = int(info.split(" ")[0])
+                    elif "bookmark" in info:
+                        bookmarks = int(info.split(" ")[0])
+                    elif "view" in info:
+                        views = int(info.split(" ")[0])
+
+            post_replies_lst.append(replies)
+            post_reposts_lst.append(reposts)
+            post_likes_lst.append(likes)
+            post_bookmarks_lst.append(bookmarks)
+            post_views_lst.append(views)
+
+            # Collecting tweet and tweet_response texts
+            tweet_text = ""
+            # Main tweet text
+            for _, child in enumerate(post_text_elements[0].children):
                 tweet_text += child.text
-            post_text.append(tweet_text)
+            post_text_lst.append(tweet_text)
+            response_tweet_text = "No response tweet"
+            if len(post_text_elements)>1:
+                # Response tweet text
+                response_tweet_text = ""
+                for _, child in enumerate(post_text_elements[1].children):
+                    response_tweet_text += child.text
+            post_response_text_lst.append(response_tweet_text)
+
+            # Checking if post has photo | video
+            post_has_video = "no"
+            post_has_photo = "no"
+            if len(post_video_elements)>0:
+                post_has_video = "yes"
+            else:
+                if len(post_photo_elements)>0:
+                    post_has_photo = "yes"
+            post_has_video_lst.append(post_has_video)
+            post_has_photo_lst.append(post_has_photo)
 
     # Compacting data into a dictionary
     data_dict = {"url":post_links_lst, 
@@ -120,11 +158,15 @@ def extract_tweets_to_df(soup):
                     "likes":post_likes_lst, 
                     "bookmarks":post_bookmarks_lst, 
                     "views":post_views_lst,
-                    "tweet-text":post_text}
+                    "tweet-text":post_text_lst,
+                    "response-tweet-text":post_response_text_lst,
+                    "has-photo":post_has_photo_lst,
+                    "has-video":post_has_video_lst}
     
     # Print for debugging
     # for key, value in data_dict.items():
     #     print(f"{key}: Len: {len(value)}")
+    # print(data_dict)
     
     df = pd.DataFrame(data_dict)
     return df
@@ -168,7 +210,7 @@ def scrape_tweets():
                     page.get_by_title('Botão \"Fazer login com o Google\"').click() # Opens a new tab
                 login_via_google(new_page_info, login_data)
 
-            time.sleep(DEFAULT_DELAY*120)
+            time.sleep(DEFAULT_DELAY*60)
             
             # Save storage state into the file.
             context.storage_state(path=AUTHENTICATED_STATE_JSON)
@@ -182,7 +224,7 @@ def scrape_tweets():
             attempts = 0
             success = False
             while attempts < MAX_ATTEMPTS:
-                date_filter = "since:2020-10-09 until:2024-04-22"
+                date_filter = f"since:{row['since']} until:{row['until']}"
                 search_str = f"{row['keywords']} {date_filter}"
                 print(f"Current search: {i} | {search_str}")
                 url_explore = "https://twitter.com/explore"
@@ -260,7 +302,7 @@ def scrape_tweets():
                 # Checking if collect went successfully
                 if success:
                     end_time = dt.datetime.now()
-                    total_collect_time = start_time - end_time
+                    total_collect_time = end_time - start_time
                     print(f"Finished collecting all tweets from - Keyword: {row['keywords']}\nTime Spent: {total_collect_time}")
                     break
             # If no errors
